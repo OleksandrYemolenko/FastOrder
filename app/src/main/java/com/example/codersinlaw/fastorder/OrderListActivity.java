8bc5f1b8703d4c5d0e13384030d6fd092c2dd360
saaho <saaho.rmolenko@gnail.com>
2018-10-21 15:36:32 +0300

    Order layout

--- app/src/main/java/com/example/codersinlaw/fastorder/OrderListActivity.java
+++ app/src/main/java/com/example/codersinlaw/fastorder/OrderListActivity.java
@@ -27,6 +27,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
